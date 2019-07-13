b800823901f2dba0ba8fcfcc136b4b2cc6cd36c2
Seungwon Eugene Jeong <economicstudio@hotmail.com>
2019-07-13 15:40:15 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,30 +1,32 @@
 const scotauto = "scotauto";
 
 $("#authorize").click(function() {
-  steem_keychain.requestAddAccountAuthority(
-    $("#username_authorize").val(),
-    scotauto,
-    "posting",
-    1,
-    function(response) {
-      console.log(response);
-    }
-  );
+  if (window.steem_keychain) {
+    steem_keychain.requestAddAccountAuthority(
+      $("#username_authorize").val(),
+      scotauto,
+      "posting",
+      1,
+      function(response) {
+        console.log(response);
+      }
+    );
+  } else {
+    alert("You need Steem Keychain.");
+  }
 });
 
 $("#revoke").click(function() {
-  steem_keychain.requestRemoveAccountAuthority(
-    $("#username_revoke").val(),
-    scotauto,
-    "posting",
-    function(response) {
-      console.log(response);
-    }
-  );
-});
-
-$(function() {
-  if (!window.steem_keychain) {
+  if (window.steem_keychain) {
+    steem_keychain.requestRemoveAccountAuthority(
+      $("#username_revoke").val(),
+      scotauto,
+      "posting",
+      function(response) {
+        console.log(response);
+      }
+    );
+  } else {
     alert("You need Steem Keychain.");
   }
 });
